38c81e289a16d9832adf2eeaae1c5a1a9ed0c3ff
yinxulai <yinxulai@hotmail.com>
2020-03-19 22:24:49 +0800

    移除 utils 使用官方库

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { join } from 'path'
-import * as utils from './utils'
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import { existsSync, readFileSync } from 'fs'
 
 // 安装依赖并执行
-function action() {
-  const platform = utils.getPlatform()
-  const pkgRoot = utils.getInput('package_root', true)
-  const githubToken = utils.getInput('github_token', true)
+async function action() {
+  const release = !!core.getInput('release', { required: false })
+  const pkgRoot = core.getInput('package_root', { required: true })
+  const githubToken = core.getInput('github_token', { required: release })
 
   const pkgJsonPath = join(pkgRoot, "package.json")
 
@@ -14,30 +15,34 @@ function action() {
   const npmLockPath = join(pkgRoot, 'package-lock.json')
 
   const pkgJson = JSON.parse(readFileSync(pkgJsonPath, "utf8"));
-  if (!pkgJson.scripts) { utils.log(`Package.json no scripts configured`) }
+  if (!pkgJson.scripts) { core.info(`Package.json no scripts configured`) }
 
-  const scriptName = utils.getInput('script_name', true) || 'publish'
-  if (!pkgJson.scripts[scriptName]) { utils.log(`Package.json no ${scriptName} of configured`) }
-
-  // 设置环境变量给后面用
-  utils.setEnv('GITHUB_TOKEN', githubToken)
-
-  // 通过检查 lock 文件判断使用哪个
-  // TODO: 万一他妈两个文件都有 但是实际用的是 yarn 就炸了
   const useNpm = existsSync(npmLockPath)
   if (useNpm) {
-    utils.log(`Will run NPM commands in directory '${pkgRoot}'`)
-    utils.run('npm install', pkgRoot)
-    utils.run('npm run publish', pkgRoot)
-    return utils.log('publish done!')
+    core.info(`Will run NPM commands in directory '${pkgRoot}'`)
+    exec.exec('npm', ['install'], { cwd: pkgRoot })
+    exec.exec('npm', ['run', 'make'], { cwd: pkgRoot })
+    if (release) {
+      await exec.exec('npm', ['run', 'publish'], {
+        cwd: pkgRoot,
+        env: { 'GITHUB_TOKEN': githubToken }
+      })
+    }
+    return core.info('done!')
   }
 
   const useYarn = existsSync(yarnLockPath)
   if (useYarn) {
-    utils.log(`Will run Yarn commands in directory '${pkgRoot}'`)
-    utils.run('yarn', pkgRoot) // 安装依赖
-    utils.run('yarn run publish', pkgRoot)
-    return utils.log('publish done!')
+    core.info(`Will run Yarn commands in directory '${pkgRoot}'`)
+    exec.exec('yarn', ['install'], { cwd: pkgRoot })
+    exec.exec('yarn', ['run', 'make'], { cwd: pkgRoot })
+    if (release) {
+      await exec.exec('yarn', ['run', 'publish'], {
+        cwd: pkgRoot,
+        env: { 'GITHUB_TOKEN': githubToken }
+      })
+    }
+    return core.info('publish done!')
   }
 }
 
