9f56cbee3990e08d9af3116e3bb1e479c4ed19e8
yinxulai <yinxulai@hotmail.com>
2020-03-19 17:49:43 +0800

    init

---
+++ src/utils.ts
@@ -0,0 +1,50 @@
+import { execSync } from "child_process"
+
+export function log(msg: string) {
+  console.log(`\n${msg}`)
+}
+
+export function exit(msg: string) {
+  console.error(msg);
+  process.exit(1);
+};
+
+// Executes the provided shell command and redirects stdout/stderr to the console
+export function run(cmd: string, cwd: string) {
+  return execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd })
+};
+
+// Determines the current operating system (one of ["mac", "windows", "linux"])
+
+type Platform = 'mac' | 'windows' | 'linux'
+
+export function getPlatform(): Platform {
+  switch (process.platform) {
+    case "win32":
+      return "windows";
+    case "darwin":
+      return "mac";
+    default:
+      return "linux";
+  }
+}
+
+export function getEnv(name: string): string | null {
+  return process.env[name.toUpperCase()] || null;
+}
+
+export function setEnv(name: string, value: string) {
+  if (value) {
+    process.env[name.toUpperCase()] = value.toString();
+  }
+}
+
+type InputName = 'github_token' | 'package_root' | 'script_name'
+
+export function getInput(name: InputName, required: boolean): string | null {
+  const value = getEnv(`INPUT_${name}`)
+  if (required && !value) {
+    exit(`"${name}" input variable is not defined`);
+  }
+  return value;
+}
