3b3309e341d19f300e265deb21f42faee8d9032a
yinxulai <yinxulai@hotmail.com>
2020-03-20 16:47:20 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,21 @@ import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import { existsSync, readFileSync } from 'fs'
 
+// 测试用的
+// process.env['INPUT_RELEASE'] = 'TEST'
+// process.env['INPUT_GITHUB_TOKEN'] = 'TEST'
+// process.env['INPUT_PACKAGE_ROOT'] = '/Users/yinxulai/Desktop/talpa'
+
 // 安装依赖并执行
 async function action() {
   const release = !!core.getInput('release', { required: false })
   const pkgRoot = core.getInput('package_root', { required: true })
   const githubToken = core.getInput('github_token', { required: release })
 
-  const pkgJsonPath = join(pkgRoot, "package.json")
-
   const yarnLockPath = join(pkgRoot, 'yarn.lock')
+  const pkgJsonPath = join(pkgRoot, "package.json")
   const npmLockPath = join(pkgRoot, 'package-lock.json')
+  const execOptions = { cwd: pkgRoot, env: { 'GITHUB_TOKEN': githubToken } }
 
   const pkgJson = JSON.parse(readFileSync(pkgJsonPath, "utf8"));
   if (!pkgJson.scripts) { core.info(`Package.json no scripts configured`) }
@@ -20,12 +25,9 @@ async function action() {
   const useNpm = existsSync(npmLockPath)
   if (useNpm) {
     core.info(`Will run NPM commands in directory '${pkgRoot}'`)
-    exec.exec('npm', ['install'], { cwd: pkgRoot })
+    await exec.exec('npm', ['install'], execOptions)
     if (release) {
-      await exec.exec('npm', ['run', 'publish'], {
-        cwd: pkgRoot,
-        env: { 'GITHUB_TOKEN': githubToken }
-      })
+      await exec.exec('npm', ['run', 'publish'], execOptions)
     }
     return core.info('done!')
   }
@@ -33,14 +35,11 @@ async function action() {
   const useYarn = existsSync(yarnLockPath)
   if (useYarn) {
     core.info(`Will run Yarn commands in directory '${pkgRoot}'`)
-    exec.exec('yarn', ['install'], { cwd: pkgRoot })
+    await exec.exec('yarn', ['install'], execOptions)
     if (release) {
-      await exec.exec('yarn', ['run', 'publish'], {
-        cwd: pkgRoot,
-        env: { 'GITHUB_TOKEN': githubToken }
-      })
+      await exec.exec('yarn', ['run', 'publish'], execOptions)
     }
-    return core.info('publish done!')
+    return core.info('done!')
   }
 }
 
